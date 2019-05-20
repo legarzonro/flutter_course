9c97f8da9713c22dfa2d6ad630a0736ec07a23cd
Leonardo Garzón <legarzonro@unal.edu.co>
2019-05-19 22:48:30 -0500

    Initial commit

---
+++ lib/product_manager.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'products.dart';
+
+class ProductManager extends StatefulWidget {
+
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
+  @override
+  _ProductManagerState createState() => _ProductManagerState();
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            color: Theme.of(context).primaryColor,
+            child: Text('Add Product'),
+            onPressed: () {
+              setState(() {
+                _products.add('Advanced Food Tester');
+              });
+            },
+          ),
+        ),
+        Products(_products)
+      ],
+    );
+  }
+}
